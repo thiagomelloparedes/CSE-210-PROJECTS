66ee881aee7c688919306005ac772c516fe121f8
thiagomelloparedes <thiago.mello.paredes@gmail.com>
2026-06-09 17:48:11 -0500

    listo

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment mathAssignment = new MathAssignment("Bob", "Algebra", "2.3", "1-10");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Sally", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
